fa91ea6730a755abd1fb5936f72c1286b3191971
satyasagar <satyasagar.vanteddu@gmail.com>
2018-11-03 22:42:49 -0500

    Testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>My name is Sagar. I am passionate about emerging technologies.</p>
     <p>I want to build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2 test</Link>
   </div>
 )
 
